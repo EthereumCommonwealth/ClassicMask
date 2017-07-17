382c77f32f125ae99e4c0a57ded02a5630180c93
Dexaran <dexaran820@gmail.com>
2017-07-17 12:37:28 +0400

    Add files via upload

---
+++ development/beefy.js
@@ -0,0 +1,19 @@
+const beefy = require('beefy')
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const port = 8124
+
+const handler = beefy({
+    entries: {'mocker.js': 'bundle.js'}
+  , cwd: __dirname
+  , live: true
+  , open: true
+  , quiet: false
+  , bundlerFlags: ['-t', 'brfs']
+})
+
+
+http.createServer(handler).listen(port)
+console.log(`Now listening on port ${port}`)
