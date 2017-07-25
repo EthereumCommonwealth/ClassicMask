cffe97890f19e5fa349550e7de17897da8f41614
coderwithattitude <coderwithattitude@gmail.com>
2017-07-25 01:01:47 +0100

    updated network id and list of providers to ETC only

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,15 +1,10 @@
-const MAINET_RPC_URL = 'https://mainnet.infura.io/metamask'
-const ROPSTEN_RPC_URL = 'https://ropsten.infura.io/metamask'
-const KOVAN_RPC_URL = 'https://kovan.infura.io/metamask'
-const RINKEBY_RPC_URL = 'https://rinkeby.infura.io/metamask'
+const ETHEREUM_CLASSIC_RPC_URL = 'https://mewapi.epool.io'
+
 
 global.METAMASK_DEBUG = 'GULP_METAMASK_DEBUG'
 
 module.exports = {
   network: {
-    mainnet: MAINET_RPC_URL,
-    ropsten: ROPSTEN_RPC_URL,
-    kovan: KOVAN_RPC_URL,
-    rinkeby: RINKEBY_RPC_URL,
+    ethereumclassic: ETHEREUM_CLASSIC_RPC_URL,
   },
 }
