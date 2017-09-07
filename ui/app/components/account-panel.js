a56ca8a1d615ec8a371e52bcbd4f91b1e7bfcbb5
Yohan Graterol <yohangraterol92@gmail.com>
2017-09-06 22:43:46 -0500

    Export identicon component

--- ui/app/components/account-panel.js
+++ ui/app/components/account-panel.js
@@ -1,7 +1,7 @@
 const inherits = require('util').inherits
 const Component = require('react').Component
 const h = require('react-hyperscript')
-const Identicon = require('./identicon')
+const Identicon = require('./identicon').default
 const formatBalance = require('../util').formatBalance
 const addressSummary = require('../util').addressSummary
 
